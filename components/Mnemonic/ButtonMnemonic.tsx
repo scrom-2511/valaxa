3cb85b6ea283f807177623c1c0c6621f2a89098d
Scrom <formain337@gmail.com>
2025-09-21 18:04:19 +0530

    made ed25519-hd-key compatible with react native

--- components/Mnemonic/ButtonMnemonic.tsx
+++ components/Mnemonic/ButtonMnemonic.tsx
@@ -1,12 +1,19 @@
-
-import { generateMnemonic } from "@/utils/Bip39";
+import { generateMnemonic, mnemonicToSeed } from "@/utils/Bip39";
+import { derivePath } from "@/utils/ed25519-hd-key";
+import { Keypair } from "@solana/web3.js";
 import React from "react";
 import { Pressable, Text } from "react-native";
 const ButtonMnemonic = ({ input, isChecked }: { input: boolean; isChecked: boolean }) => {
-  const handleOnClickCreateAccountBtn =async () => {
+  const handleOnClickCreateAccountBtn = async () => {
     if (isChecked) {
       const mnemonic = generateMnemonic();
-      console.log(mnemonic)
+      console.log(mnemonic);
+      const seed = await mnemonicToSeed(mnemonic);
+      const path = `m/44'/501'/0'/0'`;
+      const derivedSeed = derivePath(path, seed.toString("hex")).key;
+      console.log(derivedSeed)
+      const keypair = Keypair.fromSeed(derivedSeed);
+      console.log(keypair.publicKey.toBase58())
     }
   };
   const bgBtnOnCreateAccount = isChecked ? "bg-bgPrimaryBtn" : "bg-bgSecondaryBtn";
