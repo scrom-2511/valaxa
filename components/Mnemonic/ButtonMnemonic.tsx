9de4c07d6af0f8d343baae8adea75fd03e3ec5e4
Scrom <formain337@gmail.com>
2025-09-22 17:44:42 +0530

    add bitcoin wallet generator

--- components/Mnemonic/ButtonMnemonic.tsx
+++ components/Mnemonic/ButtonMnemonic.tsx
@@ -1,29 +1,12 @@
-import { generateMnemonic, mnemonicToSeed } from "@/utils/Bip39";
-import ecc from '@bitcoinerlab/secp256k1';
-import BIP32Factory from "bip32";
-import * as bitcoin from "bitcoinjs-lib";
+import { generateMnemonic } from "@/utils/Bip39";
 import React from "react";
 
 
 import { Pressable, Text } from "react-native";
 const ButtonMnemonic = ({ input, isChecked }: { input: boolean; isChecked: boolean }) => {
   const handleOnClickCreateAccountBtn = async () => {
-    const bip32 = BIP32Factory(ecc);
     if (isChecked) {
       const mnemonic = generateMnemonic();
-      // const seed = await mnemonicToSeed(mnemonic);
-      // const derivationPath = `m/44'/60'/0'/0'`;
-      // const hdNode = HDNodeWallet.fromSeed(seed);
-      // const child = hdNode.derivePath(derivationPath);
-      // const privateKey = child.privateKey;
-      // const wallet = new Wallet(privateKey);
-      // console.log(wallet)
-
-      const seed = await mnemonicToSeed(mnemonic);
-      const path = `m/84'/0'/0'/0/0`;
-      const node = bip32.fromSeed(seed, bitcoin.networks.bitcoin);
-      const child = node.derivePath(path);
-      console.log("the public key is:", child.publicKey);
     }
   };
   const bgBtnOnCreateAccount = isChecked ? "bg-bgPrimaryBtn" : "bg-bgSecondaryBtn";
--- utils/bitcoinWalletGenerator.ts
+++ utils/bitcoinWalletGenerator.ts
@@ -1,16 +1,16 @@
-// import ecc from '@bitcoinerlab/secp256k1';
-// import BIP32Factory from 'bip32';
-// import * as bitcoin from "bitcoinjs-lib";
-// import { generateMnemonic, mnemonicToSeed } from "./Bip39";
+import ecc from '@bitcoinerlab/secp256k1';
+import BIP32Factory from 'bip32';
+import * as bitcoin from "bitcoinjs-lib";
+import { generateMnemonic, mnemonicToSeed } from "./Bip39";
 
-// const bip32 = BIP32Factory(ecc);
+const bip32 = BIP32Factory(ecc);
 
 
-// export const bitcoinWalletGenerator = async (mnemonic?: string) => {
-//   let mnemonicString = mnemonic || generateMnemonic();
-//   const seed = await mnemonicToSeed(mnemonicString);
-//   const path = `m/84'/0'/0'/0/0`;
-//   const node = bip32.fromSeed(seed, bitcoin.networks.bitcoin);
-//   const child = node.derivePath(path);
-//   console.log(child.publicKey)
-// };
+export const bitcoinWalletGenerator = async (mnemonic?: string) => {
+  let mnemonicString = mnemonic || generateMnemonic();
+  const seed = await mnemonicToSeed(mnemonicString);
+  const path = `m/84'/0'/0'/0/0`;
+  const node = bip32.fromSeed(seed, bitcoin.networks.bitcoin);
+  const child = node.derivePath(path);
+  console.log(child.publicKey)
+};
