5703b10d4880ea97f3376a8ee8ded7eb1791dd6f
Scrom <formain337@gmail.com>
2025-09-22 23:34:15 +0530

    fix wallet.tsx

--- app/(tabs)/Wallet.tsx
+++ app/(tabs)/Wallet.tsx
@@ -1,14 +1,9 @@
+import MainWallet from "@/components/wallet/MainWallet";
 import React from "react";
-import { Text, View } from "react-native";
 
 const Wallet = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-bgDark">
-      <Text className="text-text text-xl font-bold mb-4">Wallet</Text>
-      <Text className="text-textMuted text-center px-8">
-        This is the Wallet tab. Navigation is working properly!
-      </Text>
-    </View>
+    <MainWallet/>
   );
 };
 
