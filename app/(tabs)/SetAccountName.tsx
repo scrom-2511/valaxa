e59fd0517b36ee482f177afc35b6c7dcec550294
Scrom <formain337@gmail.com>
2025-09-23 12:26:33 +0530

    add set account name page

---
+++ app/(tabs)/SetAccountName.tsx
@@ -0,0 +1,62 @@
+import SingleInput from "@/components/SingleInput";
+import { TokenName, WalletImgLocation } from "@/types/types";
+import { generateMnemonic } from "@/utils/Bip39";
+import { bitcoinWalletGenerator } from "@/utils/bitcoinWalletGenerator";
+import { ethereumWalletGenerator } from "@/utils/ethereumWalletGenerator";
+import { solanaWalletGenerator } from "@/utils/solanaWalletGenerator";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Account, SingleToken, useAccountStore } from "../zustand/store";
+
+const SetAccountName = () => {
+  const router = useRouter();
+  const { addAccount } = useAccountStore();
+
+  const handleOnPress = async () => {
+    const mnemonic = generateMnemonic();
+    const solanaWallet = await solanaWalletGenerator(mnemonic);
+    const ethereumWallet = await ethereumWalletGenerator(mnemonic);
+    const bitcoinWallet = await bitcoinWalletGenerator(mnemonic);
+
+    const solanaToken: SingleToken<TokenName.solana> = {
+      tokenName: TokenName.solana,
+      tokenImage: WalletImgLocation.solana,
+      amount: 10,
+      publicKey: solanaWallet.publicKey,
+    };
+    const ethreumToken: SingleToken<TokenName.ethereum> = {
+      tokenName: TokenName.ethereum,
+      tokenImage: WalletImgLocation.ethereum,
+      amount: 10,
+      publicKey: ethereumWallet.publicKey,
+    };
+    const bitcoinToken: SingleToken<TokenName.bitcoin> = {
+      tokenName: TokenName.bitcoin,
+      tokenImage: WalletImgLocation.bitcoin,
+      amount: 10,
+      publicKey: bitcoinWallet.publicKey,
+    };
+
+    const tokens = [solanaToken, ethreumToken, bitcoinToken];
+
+    const account: Account = {
+      accountName: "",
+      accountNumer: 0,
+      tokens,
+    };
+    addAccount(account);
+    router.push("/(tabs)/Wallet")
+  };
+
+  return (
+    <SingleInput
+      title="New Account Name"
+      buttonText="ADD ACCOUNT"
+      onPress={() => {handleOnPress()}}
+      placeholder="Enter the account name"
+      key="SetAccountName"
+    />
+  );
+};
+
+export default SetAccountName;
--- components/Mnemonic/ButtonMnemonic.tsx
+++ components/Mnemonic/ButtonMnemonic.tsx
@@ -1,54 +1,12 @@
-import { Account, SingleToken, useAccountStore } from "@/app/zustand/store";
-import { TokenName, WalletImgLocation } from "@/types/types";
-import { generateMnemonic } from "@/utils/Bip39";
-import { bitcoinWalletGenerator } from "@/utils/bitcoinWalletGenerator";
-import { ethereumWalletGenerator } from "@/utils/ethereumWalletGenerator";
-import { solanaWalletGenerator } from "@/utils/solanaWalletGenerator";
-import React, { useEffect } from "react";
+import { useRouter } from "expo-router";
+import React from "react";
 
 import { Pressable, Text } from "react-native";
 const ButtonMnemonic = ({ input, isChecked }: { input: boolean; isChecked: boolean }) => {
-  const { accounts, addAccount } = useAccountStore();
+  const router = useRouter();
 
-  useEffect(() => {
-    console.log(accounts);
-  }, [accounts]);
-  
   const handleOnClickCreateAccountBtn = async () => {
-    if (isChecked) {
-      const mnemonic = generateMnemonic();
-      const solanaWallet = await solanaWalletGenerator(mnemonic);
-      const ethereumWallet = await ethereumWalletGenerator(mnemonic);
-      const bitcoinWallet = await bitcoinWalletGenerator(mnemonic);
-
-      const solanaToken: SingleToken<TokenName.solana> = {
-        tokenName: TokenName.solana,
-        tokenImage: WalletImgLocation.solana,
-        amount: 10,
-        publicKey: solanaWallet.publicKey,
-      };
-      const ethreumToken: SingleToken<TokenName.ethereum> = {
-        tokenName: TokenName.ethereum,
-        tokenImage: WalletImgLocation.ethereum,
-        amount: 10,
-        publicKey: ethereumWallet.publicKey,
-      };
-      const bitcoinToken: SingleToken<TokenName.bitcoin> = {
-        tokenName: TokenName.bitcoin,
-        tokenImage: WalletImgLocation.bitcoin,
-        amount: 10,
-        publicKey: bitcoinWallet.publicKey,
-      };
-
-      const tokens = [solanaToken, ethreumToken, bitcoinToken];
-
-      const account: Account = {
-        accountName: "",
-        accountNumer: 0,
-        tokens,
-      };
-      addAccount(account);
-    }
+    if (isChecked) router.push("/(tabs)/SetAccountName")
   };
   const bgBtnOnCreateAccount = isChecked ? "bg-bgPrimaryBtn" : "bg-bgSecondaryBtn";
 
