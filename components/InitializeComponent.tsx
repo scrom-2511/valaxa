49ccfa0d44e712524eae122c6b0fe33c5d03177d
Scrom <formain337@gmail.com>
2025-10-04 11:44:28 +0530

    fix camera not showing up

--- components/InitializeComponent.tsx
+++ components/InitializeComponent.tsx
@@ -10,11 +10,15 @@ const InitializeComponent = ({ children, scrollable }: { children: React.ReactNo
   return (
     <View className="flex-1 bg-bgDark">
       <SafeAreaProvider className="flex-1 bg-bgDark">
-        <ScrollView
-          contentContainerClassName={isWeb ? "flex-1 items-center m-5 my-10" : scrollable ? "items-stretch m-5" : "flex-1 items-stretch m-5"}
-        >
-          <View className={isWeb ? "w-full max-w-[1000px] h-full px-5" : "flex-1"}>{children}</View>
-        </ScrollView>
+        {scrollable ? (
+          <ScrollView contentContainerClassName={isWeb ? "flex-1 items-center m-5 my-10" : "items-stretch m-5"}>
+            <View className={isWeb ? "w-full max-w-[1000px] h-full px-5" : "flex-1"}>{children}</View>
+          </ScrollView>
+        ) : (
+          <View className={isWeb ? "flex-1 items-center justify-center m-5 my-10" : "flex-1 items-stretch m-5"}>
+            <View className={isWeb ? "w-full max-w-[1000px] h-full px-5" : "flex-1"}>{children}</View>
+          </View>
+        )}
       </SafeAreaProvider>
     </View>
   );
