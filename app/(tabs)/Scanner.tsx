dabb75ff2949d9ba3d9f13a940ecf1b65430da3a
Scrom <formain337@gmail.com>
2025-10-06 12:57:24 +0530

    improve scanner.tsx

--- app/(tabs)/Scanner.tsx
+++ app/(tabs)/Scanner.tsx
@@ -1,41 +1,59 @@
-import InitializeComponent from '@/components/InitializeComponent';
-import { CameraView, useCameraPermissions } from 'expo-camera';
-import { useRouter } from 'expo-router';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
-import { useSingleInputStore } from '../zustand/store';
+import HeaderCommon from "@/components/HeaderCommon";
+import InitializeComponent from "@/components/InitializeComponent";
+import { CameraView, useCameraPermissions } from "expo-camera";
+import { useRouter } from "expo-router";
+import React from "react";
+import { Button, Text, View } from "react-native";
+import { useSingleInputStore } from "../zustand/store";
 
 const Scanner = () => {
   const [permission, requestPermission] = useCameraPermissions();
   const router = useRouter();
   const { setCurrentInput } = useSingleInputStore();
+
   const handleOnBarcodeScanned = () => {
-    setCurrentInput(null)
-    router.push("/(tabs)/Send")
-  }
+    setCurrentInput(null);
+    router.push("/(tabs)/Send");
+  };
 
   if (!permission) {
-    // Camera permissions are still loading.
     return <View />;
   }
 
   if (!permission.granted) {
-    // Camera permissions are not granted yet.
-    return (                    
-      <View className='flex-1 justify-center'>
-        <Text>We need your permission to show the camera</Text>
-        <Button onPress={requestPermission} title="grant permission" />
+    return (
+      <View className="flex-1 justify-center items-center px-5">
+        <Text className="text-center mb-4">
+          Camera access is required to scan QR codes.
+        </Text>
+        <Button onPress={requestPermission} title="Grant Permission" />
       </View>
     );
   }
 
   return (
     <InitializeComponent scrollable={false}>
-        <Text>Camera component</Text>
-      <CameraView style={{flex:1}} facing="back" onBarcodeScanned={(e)=>{console.log(e)}} />
+      <HeaderCommon
+        title="Scan QR Code"
+        placeholder="Align the QR code within the frame to scan"
+      />
+
+      <CameraView
+        style={{ flex: 1, marginVertical: 20 }}
+        facing="back"
+        onBarcodeScanned={(e) => {
+          console.log(e); // You may want to call handleOnBarcodeScanned here
+        }}
+      />
+
+      <View className="flex items-center mb-20">
+        <Text className="text-text my-10">OR</Text>
+        <Text className="text-text underline">
+          I want to enter the public key manually
+        </Text>
+      </View>
     </InitializeComponent>
   );
+};
 
-}
-
-export default Scanner
+export default Scanner;
